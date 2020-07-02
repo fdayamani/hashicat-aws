fac5bb26a6516ae87443a125060d1bcc4ef1b2b0
fdayamani <fdayamani@users.noreply.github.com>
2020-07-02 11:57:31 +0100

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Here's a picture of the cutest cat in the world! <Enter some standard marketing jargon here>.
   <!-- END -->
 
   </div>
